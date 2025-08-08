4773e48a43198b2dc37f5a182410a650d543ca1f
Stefano Di Pasquale <stefaniscion@gmail.com>
2025-08-08 05:56:21 +0200

    add new partners

--- src/lib/section/SectionPartners.svelte
+++ src/lib/section/SectionPartners.svelte
@@ -1,13 +1,16 @@
 <!-- Partners -->
 <section class="bg-neutral-950 text-neutral-100 py-20 font-mona-sans">
     <div class="container mx-auto px-4">
-        <h2 class="text-5xl mb-5 text-center font-semibold"><span class="text-amber-600">Ho lavorato</span> con</h2>
+        <h2 class="text-5xl mb-5 text-center font-semibold"><span class="text-amber-600">Ho collaborato</span> con</h2>
         <div class="flex flex-wrap justify-between">
-            <img class="w-full basis-1/2 sm:basis-1/5" alt="Aeronautica Militare" src="img/logo/aeronautica_militare.png">
-            <img class="w-full basis-1/2 sm:basis-1/5" alt="Anas" src="img/logo/anas.png">
-            <img class="w-full basis-1/2 sm:basis-1/5" alt="Altran" src="img/logo/altran.png">
-            <img class="w-full basis-1/2 sm:basis-1/5" alt="Dedagroup" src="img/logo/dedagroup.png">
-            <img class="w-full basis-1/2 sm:basis-1/5" alt="Giddierre" src="img/logo/giddierre.png">
+            <img class="w-full p-2 basis-1/2 sm:basis-1/4" alt="Aeronautica Militare" src="img/logo/aeronautica_militare.png">
+            <img class="w-full p-2 basis-1/2 sm:basis-1/4" alt="Anas" src="img/logo/anas.png">
+            <img class="w-full p-2 basis-1/2 sm:basis-1/4" alt="Altran" src="img/logo/altran.png">
+            <img class="w-full p-2 basis-1/2 sm:basis-1/4" alt="Dedagroup" src="img/logo/dedagroup.png">
+            <img class="w-full p-2 basis-1/2 sm:basis-1/4" alt="Giddierre" src="img/logo/giddierre.png">
+            <img class="w-full p-2 basis-1/2 sm:basis-1/4" alt="Credit Agricole" src="img/logo/credit-agricole.png">
+            <img class="w-full p-2 basis-1/2 sm:basis-1/4" alt="PyxWeb" src="img/logo/pyxweb.png">
+            <img class="w-full p-2 basis-1/2 sm:basis-1/4" alt="BCC" src="img/logo/bcc.png">
         </div>
     </div>
 </section>
