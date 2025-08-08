e2ad5e6fc3075a479abbc7dd4b556f1b206e2567
Stefano Di Pasquale <stefaniscion@gmail.com>
2025-08-08 19:00:39 +0200

    add html title

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,12 @@
-<script>
+<script lang="ts">
     import "../app.css";
     import "devicon/devicon.min.css";
+    let title: string = "Stefano Di Pasquale | Software Engineer & Developer";
 </script>
+
+
+<svelte:head>
+	<title>{title}</title>
+</svelte:head>
+
 <slot/>
