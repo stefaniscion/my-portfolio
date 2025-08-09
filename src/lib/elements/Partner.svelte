2f69597c0206f88e471a8b8faab9ad68a67f757c
Stefano Di Pasquale <stefaniscion@gmail.com>
2025-08-09 16:21:34 +0200

    partner modularization

---
+++ src/lib/elements/Partner.svelte
@@ -0,0 +1,6 @@
+<script lang="ts">
+    export let name: string;
+    export let url: string;
+</script>
+
+<img class="w-full p-2 basis-1/2 sm:basis-1/4" alt={name} src={url}>
--- src/lib/section/SectionPartners.svelte
+++ src/lib/section/SectionPartners.svelte
@@ -1,16 +1,20 @@
+<script lang="ts">
+    import Partner from "$lib/elements/Partner.svelte";
+</script>
+
 <!-- Partners -->
 <section class="bg-neutral-950 text-neutral-100 py-20 font-mona-sans">
     <div class="container mx-auto px-4">
         <h2 class="text-5xl mb-5 text-center font-semibold"><span class="text-amber-600">Ho collaborato</span> con</h2>
         <div class="flex flex-wrap justify-between">
-            <img class="w-full p-2 basis-1/2 sm:basis-1/4" alt="Aeronautica Militare" src="img/logo/aeronautica_militare.png">
-            <img class="w-full p-2 basis-1/2 sm:basis-1/4" alt="Anas" src="img/logo/anas.png">
-            <img class="w-full p-2 basis-1/2 sm:basis-1/4" alt="Altran" src="img/logo/altran.png">
-            <img class="w-full p-2 basis-1/2 sm:basis-1/4" alt="Dedagroup" src="img/logo/dedagroup.png">
-            <img class="w-full p-2 basis-1/2 sm:basis-1/4" alt="Giddierre" src="img/logo/giddierre.png">
-            <img class="w-full p-2 basis-1/2 sm:basis-1/4" alt="Credit Agricole" src="img/logo/credit-agricole.png">
-            <img class="w-full p-2 basis-1/2 sm:basis-1/4" alt="PyxWeb" src="img/logo/pyxweb.png">
-            <img class="w-full p-2 basis-1/2 sm:basis-1/4" alt="BCC" src="img/logo/bcc.png">
+            <Partner name="Aeronautica Militare" url="img/logo/aeronautica_militare.png"></Partner>
+            <Partner name="Anas" url="img/logo/anas.png"></Partner>
+            <Partner name="Altran" url="img/logo/altran.png"></Partner>
+            <Partner name="Dedagroup" url="img/logo/dedagroup.png"></Partner>
+            <Partner name="Giddierre" url="img/logo/giddierre.png"></Partner>
+            <Partner name="Credit Agricole" url="img/logo/credit-agricole.png"></Partner>
+            <Partner name="PyxWeb" url="img/logo/pyxweb.png"></Partner>
+            <Partner name="BCC" url="img/logo/bcc.png"></Partner>
         </div>
     </div>
 </section>
