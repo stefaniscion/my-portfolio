3def3eff3323993f0528e234c4e3f6cf688ad8b6
Stefano Di Pasquale <stefaniscion@gmail.com>
2025-08-09 18:59:20 +0200

    move tag manager

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,23 +1,11 @@
-
-<svelte:head>
-	<title>{title}</title>
-	<!-- Script di caricamento gtag (può stare qui perché è solo il loader) -->
-	<script async src="https://www.googletagmanager.com/gtag/js?id=G-PKN7TG90EK"></script>
-</svelte:head>
-
 <script lang="ts">
     import "../app.css";
     import "devicon/devicon.min.css";
     let title: string = "Stefano Di Pasquale | Software Engineer & Developer";
-
-    import { onMount } from "svelte";
-
-	onMount(() => {
-		window.dataLayer = window.dataLayer || [];
-		function gtag(){ dataLayer.push(arguments); }
-		gtag('js', new Date());
-		gtag('config', 'G-PKN7TG90EK');
-	});
 </script>
+
+<svelte:head>
+	<title>{title}</title>
+</svelte:head>
 
 <slot/>
