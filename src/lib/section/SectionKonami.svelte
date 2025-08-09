06e2358486a46eb3d70dc72ac7a7a494ae8990cc
Stefano Di Pasquale <stefaniscion@gmail.com>
2025-08-09 17:31:53 +0200

    edit konami section

--- src/lib/section/SectionKonami.svelte
+++ src/lib/section/SectionKonami.svelte
@@ -1,6 +1,4 @@
 <!-- Konami -->
-<section class="bg-neutral-950 text-neutral-100 py-2 font-mona-sans">
-    <div class="container mx-auto">
-        <img src="img/egg/konami-code.svg" alt="Konami Code">
-    </div>
+<section class="bg-neutral-950 text-neutral-800 py-2 font-mona-sans">
+    <p class="text-center text-4xl">↑ ↑ ↓ ↓ ← → ← → B A</p>
 </section>
