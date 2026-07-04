2b8d1463efc6f087daabda4eeec53a6c2775e55b
Stefano Di Pasquale <stefaniscion@gmail.com>
2026-07-05 00:13:49 +0200

    increase partner logo size

--- src/lib/elements/Partner.svelte
+++ src/lib/elements/Partner.svelte
@@ -9,7 +9,7 @@
 </script>
 
 <div class="basis-1/2 sm:basis-1/4 p-4">
-    <div class="flex h-28 sm:h-32 items-center justify-center">
+    <div class="flex w-64 items-center justify-center">
         <img class="max-h-full max-w-full object-contain" alt={name} src={url}>
     </div>
 </div>
